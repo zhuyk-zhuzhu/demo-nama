b674629ff36d872eb395debaffeb43a9174184b6
zhuyk-zhuzhu <wy1352672149@163.com>
2020-01-10 16:15:16 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        System.out.println("Hello World4");
+        System.out.println("Hello World2");
+        System.out.println("Hello World32");
+        System.out.println("Hello World5");
+    }
+}
